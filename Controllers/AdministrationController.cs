cfaa5341c847bfea6d8b36ab0d14a1532bd23753
Patrick Lafferty <patrick_lafferty@outlook.com>
2017-01-16 14:53:04 -0500

    Administration/Edit can add/remove roles to userse

--- Controllers/AdministrationController.cs
+++ Controllers/AdministrationController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using SpaceYYZ.Models;
 using SpaceYYZ.Models.AdministrationViewModels;
 
@@ -14,11 +15,14 @@ namespace SpaceYYZ.Controllers
 	public class AdministrationController : Controller
 	{
 		private readonly UserManager<ApplicationUser> _userManager;
+		private readonly RoleManager<IdentityRole> _roleManager;
 
 		public AdministrationController(
-				UserManager<ApplicationUser> userManager)
+				UserManager<ApplicationUser> userManager,
+				RoleManager<IdentityRole> roleManager)
 		{
 			_userManager = userManager;
+			_roleManager = roleManager;
 		}
 
 		[HttpGet]
@@ -83,12 +87,20 @@ namespace SpaceYYZ.Controllers
 				}
 				else
 				{
+
+					var availableRoles = _roleManager.Roles.Select(r => new SelectListItem {
+							Value = r.Name,
+							Text = r.Name
+						}).ToList();
+
 					var model = new UserDetailsViewModel() {
 						Username = user.UserName,
 						Id = id,
 						FirstName = user.FirstName,
 						LastName = user.LastName,
-						Roles = await _userManager.GetRolesAsync(user)
+						Roles = await _userManager.GetRolesAsync(user),
+						AvailableRoles = availableRoles
+						
 					};
 
 					return View(model);
@@ -132,6 +144,65 @@ namespace SpaceYYZ.Controllers
 				return View();
 			}
 		}
+
+		[HttpGet]
+		public async Task<IActionResult> Roles(string userId)
+		{
+			if (userId == null)
+			{
+				return NotFound();
+			}
+			else
+			{
+				var user = await _userManager.FindByIdAsync(userId);
+
+				var availableRoles = _roleManager.Roles.Select(r => new SelectListItem {
+						Value = r.Name,
+						Text = r.Name
+						}).ToList();
+
+				var model = new RolesViewModel() {
+					Username = user.UserName,
+					Id = userId,
+					NewRole = availableRoles[0].Value,
+					AvailableRoles = availableRoles
+
+				};
+
+				return View(model);
+			}
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> AddRole(RolesViewModel model)
+		{
+			foreach(var a in ModelState)
+			{
+				foreach(var e in a.Value.Errors)
+				{
+					System.Console.WriteLine(e.ErrorMessage);
+				}
+			}
+
+			if (ModelState.IsValid)
+			{
+				var user = await _userManager.FindByIdAsync(model.Id);
+
+				var result = await _userManager.AddToRoleAsync(user, model.NewRole);
+
+				if (result.Succeeded)
+				{
+					return RedirectToAction("Edit", new { id = user.Id });
+				}
+				else
+				{
+				
+				}
+			}
+
+			return View();
+		}
+
 
 	}
 }
--- Models/AdministrationViewModels/RolesViewModel.cs
+++ Models/AdministrationViewModels/RolesViewModel.cs
@@ -1,25 +1,15 @@
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace SpaceYYZ.Models.AdministrationViewModels
 {
-	public class UserDetailsViewModel
+	public class RolesViewModel
 	{
-		[Required]
 		public string Username {get; set;}
-
-		[Required]
 		public string Id {get; set;}
-
-		[Required]
-		[Display(Name = "First Name")]
-		public string FirstName {get; set;}
-
-		public string LastName {get; set;}
-
-		public IList<string> Roles {get; set;} 
+		
+		public string NewRole {get; set;}
 		public List<SelectListItem> AvailableRoles {get; set;}
 	}
 }
