97103fa397c98b6804fb8c648c01aca12e217a87
Patrick Lafferty <patrick_lafferty@outlook.com>
2017-02-07 18:19:57 -0500

    Worked on validating Admin/Edit by adding attributes to UserDetailsViewModel and setting up the scripts for client side validation

--- Models/AdministrationViewModels/UserDetailsViewModel.cs
+++ Models/AdministrationViewModels/UserDetailsViewModel.cs
@@ -14,9 +14,13 @@ namespace SpaceYYZ.Models.AdministrationViewModels
 		public string Id {get; set;}
 
 		[Required]
+		[RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Only letters are allowed")]
 		[Display(Name = "First Name")]
 		public string FirstName {get; set;}
 
+		[Required]
+		[RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Only letters are allowed")]
+		[Display(Name = "Last Name")]
 		public string LastName {get; set;}
 
 		public IList<string> Roles {get; set;} 
